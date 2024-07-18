079908bd9b6f6d224a4dc56cb9287355fc4dec41
boseji <boseji@users.noreply.github.com>
2024-07-18 09:08:33 +0530

    Initial migration

--- target/n76/n76e616.go
+++ target/n76/n76e616.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,8 +16,8 @@ package n76
 import (
 	"errors"
 
-	"github.com/erincandescent/nuvoprog/protocol"
-	"github.com/erincandescent/nuvoprog/target"
+	"github.com/boseji/nuvoprog/protocol"
+	"github.com/boseji/nuvoprog/target"
 )
 
 //go:generate enumer -type=BootSelect -trimprefix=BootFrom -transform=snake -json -text
